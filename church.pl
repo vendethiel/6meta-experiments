b98d32f4209d6d0b6355ccf242f733913886c390
vendethiel <vendethiel@hotmail.fr>
2015-06-20 13:57:13 +0200

    Use defaulted type parameters (broken still)
    And might never work :). But this version looks much cleaner

--- church.pl
+++ church.pl
@@ -13,34 +13,8 @@ subset _3 of Suc[_2];
 
 say count(Suc[Suc[Suc[Zero]]]); #=> 3
 
-role Add[::T1, ::T2] { ... }
-role Mul[::T1, ::T2] { ... }
-subset Op where Add|Mul;
-subset NatOp where Nat|Op;
+role Add[Zero, ::N, Result = N] {}
 
-role Add[Zero, Zero] {
-  method count { Zero };
-}
-role Add[Zero, NatOp ::N] {
-  method count { N.count }
-}
-role Add[NatOp ::N, Zero] {
-  method count { N.count }
-}
-role Add[NatOp ::N, NatOp ::N2] {
-  method count { N2.count }
-}
+role Add[Suc[::N], ::N2, Result = Add[N, Suc[N2]].Result] {}
 
-say Add[_2, Add[_1, _3]].count; #=> 9
-
-role Mul[Zero, NatOp ::N] {
-  method count { 0 };
-}
-role Mul[NatOp ::N, Zero] {
-  method count { 0 }
-}
-role Mul[NatOp ::N, NatOp ::N2] {
-  method count { N.count * N2.count }
-}
-
-say Mul[_3, Add[_1, Mul[_2, _2]]].count; #=> 15
+say count(Add[_3, _1].Result);
